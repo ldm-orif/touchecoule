92e0d5d380f4e1276e4ae9b097292a037efac5d2
ldm-orif <Luca.Dias@formation.orif.ch>
2026-05-06 14:34:37 +0200

    Implemented BOARD class.

--- board.py
+++ board.py
@@ -2,4 +2,30 @@
 # CREATION : 30.04.2026
 # contains board logic and data
 
-# MISSING : MANAGEMENT OF A SINGLE PLAYER'S GIRD | CURRENTLY SELECTED CASE | CASE STATES (ALL OF THEM) | SHIP PLACEMENTS | SHIPS LEFT | COLLISIONS
+from enum import Enum
+from constants import ORIENTATION
+
+class Case_State(Enum):
+    CLEAN = 0
+    MISS = 1
+    HIT = 1
+    SUNK = 2
+
+class BOARD:
+    def __init__(self, dimension):
+        self.dimension = dimension
+        self.grid = [[Case_State.CLEAN for x in range(dimension)] for y in range(dimension)] # [h][w]
+    
+    def miss(self, case):
+        self.grid[case[0]][case[1]] = Case_State.MISS
+
+    def hit(self, case):
+        self.grid[case[0]][case[1]] = Case_State.HIT
+
+    def sink(self, case, length, orientation):
+        if orientation == ORIENTATION.HORIZONTAL:
+            for i in range(length):
+                self.grid[case[0]][case[1]+i] = Case_State.SUNK
+        if orientation == ORIENTATION.VERTICAL:
+            for i in range(length):
+                self.grid[case[0]+i][case[1]] = Case_State.SUNK
