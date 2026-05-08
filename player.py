336838da22e305c408e8bf0c32ffd04bdedd8ea9
ldm-orif <Luca.Dias@formation.orif.ch>
2026-05-08 15:52:19 +0200

    Corrected some PLAYER method's attribute.

--- player.py
+++ player.py
@@ -12,7 +12,7 @@ class PLAYER:
         self.board = BOARD(GRID_SIZE)
         self.ships = [SHIP(x) for x in range(SHIP_PER_PLAYER)]
         self.selected_case = Vector2(-1,-1) # indicates where the player aimed on ANY grid, not specifically his own
-        self.selected_ship = -1
+        self.selected_ship = 0
 
     def nb_ship_unsunk(self):
         nb_unsunk = 0
@@ -21,37 +21,43 @@ class PLAYER:
                 nb_unsunk += 1
         return nb_unsunk
     
-    def launch(self, aimed_case): # return True if a ship is hit, return False if not
+    def launch(self): # return True if a ship is hit, return False if not
         for ship in self.ships:
             if ship.orientation == ORIENTATION.HORIZONTAL:
-                if aimed_case.y == ship.position.y:
+                if self.selected_case.y == ship.position.y:
                     for i in range(ship.length):
-                        if aimed_case.x == ship.position.x +i:
-                            ship.hit(aimed_case)
+                        if self.selected_case.x == ship.position.x +i:
+                            ship.hit(self.selected_case)
                             if ship.sunk:
                                 self.board.sink(ship.position, ship.length, ship.orientation)
                             self.selected_case = Vector2(-1,-1)
                             return True
             if ship.orientation == ORIENTATION.VERTICAL:
-                if aimed_case.x == ship.position.x:
+                if self.selected_case.x == ship.position.x:
                     for i in range(ship.length):
-                        if aimed_case.y == ship.position.y +i:
-                            ship.hit(aimed_case)
+                        if self.selected_case.y == ship.position.y +i:
+                            ship.hit(self.selected_case)
                             if ship.sunk:
                                 self.board.sink(ship.position, ship.length, ship.orientation)
                             self.selected_case = Vector2(-1,-1)
                             return True
-        self.board.miss(aimed_case)
+        self.board.miss(self.selected_case)
         return False
     
     def aim(self, case): # change selected case, return True if case is a valid target (inside grid, position never launched at), False otherwise
-        a=1
+        self.selected_case = case
 
-    def place(self, ship_type, orientation):
-        a=1
+    def select(self, variant):
+        self.selected_ship = variant
 
-    def remove(self, ship_type):
-        a=1
+    def place(self, case): # try to place selected ship at indicated case without causing a collision
+        a=1 #TODO CHECK FOR COLLISIONS
+
+    def rotate(self, to_the_right): # try to rotate selected ship without causing a collision, can freely rotate unplaced ships
+        a=1 #TODO CHECK FOR COLLISIONS
+
+    def remove(self, variant):
+        a=1 #TODO
 
     def collides(self): # check if selected_ship ship collides with other ships
-        a=1
+        a=1 #TODO
