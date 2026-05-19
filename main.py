3d1a63ff3b3181ac1b40871f703efc6ff54be839
ldm-orif <Luca.Dias@formation.orif.ch>
2026-05-19 14:53:52 +0200

    Reduced AI timer by 500ms to reduce boredom.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 AI_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(AI_UPDATE,2000)
+pygame.time.set_timer(AI_UPDATE,1500)
 
 game = GAME(screen)
 
